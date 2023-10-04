00b5e584827fc1b921069248ffde383e542f7704
saiteja0711 <saiteja7112001@gmail.com>
2023-10-04 12:21:57 +0530

    using map in a array

--- play.js
+++ play.js
@@ -1,11 +1,22 @@
-const productOfNumbers=(a,b)=>a*b;
-console.log(productOfNumbers(5,8));
-const Student={
-    name: 'raju',
-    age:25,
-    greet(){
-        console.log('Hi I am '+this.name);
-    }
+// const productOfNumbers=(a,b)=>a*b;
+// console.log(productOfNumbers(5,8));
+// const Student={
+//     name: 'raju',
+//     age:25,
+//     greet(){
+//         console.log('Hi I am '+this.name);
+//     }
 
-};
-Student.greet();
+// };
+// Student.greet();
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+const editedArray= array.map(fruits=>{
+    if(fruits===' ')
+    {
+        return 'empty string'
+    }
+    else{
+        return fruits;
+    }
+})
+console.log(editedArray);

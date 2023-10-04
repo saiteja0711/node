8a4b3024e84e0e22d1c836e3a68f8a7dfd1d19c9
saiteja0711 <saiteja7112001@gmail.com>
2023-10-04 11:42:32 +0530

    product and student object code

---
+++ play.js
@@ -0,0 +1,11 @@
+const productOfNumbers=(a,b)=>a*b;
+console.log(productOfNumbers(5,8));
+const Student={
+    name: 'raju',
+    age:25,
+    greet(){
+        console.log('Hi I am '+this.name);
+    }
+
+};
+Student.greet();

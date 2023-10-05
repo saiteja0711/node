7c8362747922481b7a8c4a6c3bba66cd36ece962
saiteja0711 <saiteja7112001@gmail.com>
2023-10-05 12:08:06 +0530

    printing message on web page

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
 const http = require('http');
+const fs= require('fs');
 
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    const method=req.method;
+    if(url === '/'){
+    
+    
+    return fs.readFile("message.txt" ,(err, data) => {
+        if(err)
+        {
+            console.log("error");
+            fs.writeFileSync('message.txt',"data");
+           
+        }
+        
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')  
+         res.write(`<body>${data}</body>`);
+       
+    
+    res.write('<body><form action ="/message"  method ="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>')
+    return res.end();
+});
+    }
+    if(url === '/message' && method ==='POST')
+    {
+        const body = [];
+        req.on('data', (chunk) =>{
+         console.log(chunk);
+         body.push(chunk);
+        });
+       return req.on( 'end' ,() => {
+         const parsedBody = Buffer.concat(body).toString();
+         const message= parsedBody.split('=')[1];
+         fs.writeFile('message.txt',message,(err) => {
+            res.statusCode=302;
+            res.setHeader('Location','/')
+            return res.end();
+        });
+        
+        });
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
-    res.write('<head><title>first page</title></head>')
-    if (req.url === '/home')
-    {
-     res.write('<body><h1>Welcome home</h1></body>');
-    }
-    if (req.url === '/about')
-    {
-     res.write('<body><h1>Welcome to About us page</h1></body>');
-    }
-    if (req.url === '/node')
-    {
-     res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-    }
+    res.write('<head><title>First page</title></head>')
+    res.write('<body><h1> Hello from Node JS</h1></body>');
     res.write('</html>')
     res.end();
 });

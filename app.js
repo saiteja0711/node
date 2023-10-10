b19de565a1b1ba08b929fcdaf2ace65f24682820
saiteja0711 <saiteja7112001@gmail.com>
2023-10-10 15:39:29 +0530

    clean up code

--- app.js
+++ app.js
@@ -15,24 +15,35 @@ const app= express();
 //     console.log("This always runs!")
 //     next();
 // })
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
  app.use(bodyparser.urlencoded({extended: false}));
 
-app.use ('/add-product',(req,res,next) => {
-    //console.log('In another middleware :');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+// app.use ('/add-product',(req,res,next) => {
+//     //console.log('In another middleware :');
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
    
-});
+// });
 
-app.post('/product',(req,res,next) => {
-     console.log(req.body); 
-    res.redirect('/');
-})
+// app.post('/product',(req,res,next) => {
+//      console.log(req.body); 
+//     res.redirect('/');
+// })
 
-app.use ('/',(req,res,next) => {
-    //console.log('In another middleware :');
-    res.send('<h1> Hello from Express</h1>');
-    //res.send( { key1: value })
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+
+// app.use ('/',(req,res,next) => {
+//     //console.log('In another middleware :');
+//     res.send('<h1> Hello from Express</h1>');
+//     //res.send( { key1: value })
+// });
+ app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+ })
 
 
 //const server=http.createServer(app);
